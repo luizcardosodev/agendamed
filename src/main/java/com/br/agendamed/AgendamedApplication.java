6d54c99e95dd49017f06ecaafe17b7987acbaf2f
luizcardosodev <luizcardosodev@gmail.com>
2026-04-21 18:01:34 -0300

    first commit.

---
+++ src/main/java/com/br/agendamed/AgendamedApplication.java
@@ -0,0 +1,13 @@
+package com.br.agendamed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/agendamed/AgendamedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.agendamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
